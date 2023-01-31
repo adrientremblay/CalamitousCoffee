0e12d7fef664bded26f29af19c0b6fdd46f6e1b4
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-30 23:29:22 -0500

    Added a query

--- create_database.sql
+++ create_database.sql
@@ -2,13 +2,16 @@
 DROP DATABASE IF EXISTS calamitous_coffee;
 
 -- Create the database
-create database calamitous_coffee;
+CREATE database calamitous_coffee;
 
 -- Activate the database
 use calamitous_coffee;
 
 -- Creating a table for coffee
-create table coffee_table(id int, name varchar(255), region varchar(255), roast varchar(255));
+CREATE table coffee_table(id int, name varchar(255), region varchar(255), roast varchar(255));
 
 -- Inserting a record into the coffee table
-insert into coffee_table values (1, "brown water", "montreal", "medium");
+INSERT into coffee_table values (1, "brown water", "montreal", "medium");
+
+-- Querying everything from the coffee table
+SELECT * FROM coffee_table;
