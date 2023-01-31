479e7e17886dd67bf7cdeb31b9f2e3d52c9c6193
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-30 22:49:17 -0500

    Script to create the database

--- create_database.sql
+++ create_database.sql
@@ -1,1 +1,14 @@
+-- We want to start fresh, so delete the database if it already exists
+DROP DATABASE IF EXISTS calamitous_coffee;
+
+-- Create the database
 create database calamitous_coffee;
+
+-- Activate the database
+use calamitous_coffee;
+
+-- Creating a table for coffee
+create table coffee_table(id int, name varchar(255), region varchar(255), roast varchar(255));
+
+-- Inserting a record into the coffee table
+insert into coffee_table values (1, "brown water", "montreal", "medium");
