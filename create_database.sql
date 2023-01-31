8d4542381f0e94503cbfb520a1ffd1db692eb59d
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-31 00:22:42 -0500

    Add code to create and query the customers table

--- create_database.sql
+++ create_database.sql
@@ -17,3 +17,13 @@ INSERT into coffee_table values (3, "5 hour energy shot", "compton", "light");
 INSERT into coffee_table values (4, "toxic espresso", "okinawa", "medium");
 INSERT into coffee_table values (5, "vodka", "st. petersburg", "light");
 INSERT into coffee_table values (6, "liquid caffeine", "oblivion", "heavy");
+
+-- Creating a table for customers
+CREATE table customer_table(id int, first_name varchar(255), last_name varchar(255), origin varchar(255), age int, alias varchar(255));
+
+-- Populating customers table
+INSERT into customer_table values (1, "michael", "shillyshally", "calgary", 20, "jester");
+INSERT into customer_table values (2, "hugh", "mungus", "calgary", 49, "mastadon");
+INSERT into customer_table values (3, "ragnar", "ragnarson", "calgary", 300, "viking");
+INSERT into customer_table values (4, "bill", "bill", "calgary", 34, "killer");
+INSERT into customer_table values (5, "bob", "nightblood", "calgary", 109, "death");
--- queries.sql
+++ queries.sql
@@ -3,3 +3,6 @@ SELECT * FROM coffee_table;
 
 -- Querying only the names from the coffee table
 SELECT name from coffee_table;
+
+-- Querying everything from the customers table
+SELECT * FROM customer_table;
